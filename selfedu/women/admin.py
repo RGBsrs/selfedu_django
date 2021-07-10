b21fb3a5dcdaa167252494de25dbe0ef9f02fe8e
Dmytro <grjanko.d.a@gmail.com>
2021-07-10 20:04:33 +0300

    Admin model for categories

--- selfedu/women/admin.py
+++ selfedu/women/admin.py
@@ -6,6 +6,10 @@ class WomenAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
 # Register your models here.
-admin.site.register(Women)
-admin.site.register(Category)
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
