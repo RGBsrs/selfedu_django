f67c0aba41615895b5c369fa811e0896c0fefc62
Dmytro <grjanko.d.a@gmail.com>
2021-07-10 17:06:43 +0300

    add templates and base styles

--- selfedu/women/views.py
+++ selfedu/women/views.py
@@ -1,11 +1,15 @@
 from django.http.response import HttpResponseNotFound
 from django.shortcuts import redirect, render
 
+from .models import Women
+
 # Create your views here.
+menu = ['About', 'Add post', 'Contacts', 'Log in']
 
 
 def index(request):
-    return render(request, 'women/index.html')
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'menu' : menu, 'title' : 'Main', 'posts' : posts})
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("Страница не найдена")
