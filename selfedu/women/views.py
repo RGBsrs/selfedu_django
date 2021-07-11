eea542127dec3180b59c82a03a74dc75c1d94866
Dmytro <grjanko.d.a@gmail.com>
2021-07-11 12:25:21 +0300

    add initial tag for cat list

--- selfedu/women/views.py
+++ selfedu/women/views.py
@@ -1,5 +1,5 @@
 from django.http.response import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Category, Women
 
@@ -14,10 +14,8 @@ menu = [
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts' : posts,
-        'cats' : cats,
         'menu' : menu, 
         'title' : "Главная страница", 
         'cat_selected': 0,
@@ -37,18 +35,26 @@ def login(request):
     return HttpResponse("logged in")
 
 def show_post(request, post_id):
-    return HttpResponse(f"Post with pk - {post_id}")
+    post = get_object_or_404(Women, pk = post_id)
+
+    context = {
+        'post' : post,
+        'menu' : menu, 
+        'title' : post.title, 
+        'cat_selected': 1,
+    }
+
+    return render(request, 'women/post.html', context= context)
+
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id = cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts' : posts,
-        'cats' : cats,
         'menu' : menu, 
         'title' : "Отображение по рубрикам", 
         'cat_selected': cat_id,
