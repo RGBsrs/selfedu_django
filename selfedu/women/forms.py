e553afa7197ee4a0ae71e908d4ecd485c732db75
Dmytro <grjanko.d.a@gmail.com>
2021-09-03 09:09:32 +0300

    save fixes

--- selfedu/women/forms.py
+++ selfedu/women/forms.py
@@ -1,7 +1,24 @@
 from django import forms
-from .models import Women, Category
+from django.core.exceptions import ValidationError
+from .models import Women
 
 class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = "Категория не выбрана"
+
     class Meta:
         model = Women
-        fields = ['title', 'slug', 'content', 'is_published', 'cat']
+        fields = ['title', 'slug', 'content','photo', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-im=nput'}),
+            'slug': forms.TextInput(attrs={'class':'form-im=nput'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина поля больше 200 символов')
+        
+        return title
