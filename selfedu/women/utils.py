c9c0da1e254c5ef637bae3de90da0568ed721482
Dmytro <grjanko.d.a@gmail.com>
2021-10-12 15:31:35 +0300

    add auth logic to data mixin

--- selfedu/women/utils.py
+++ selfedu/women/utils.py
@@ -1,5 +1,6 @@
-from .models import Category
+from django.db.models import Count
 
+from .models import Category
 
 menu = [
     {'title': "О сайте", 'url_name' : 'about'},
@@ -12,8 +13,13 @@ menu = [
 class DataMixin:
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.all()
-        context['menu'] = menu
+        cats = Category.objects.annotate(Count('women'))
+
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(1)
+
+        context['menu'] = user_menu
         context['cats'] = cats
         if 'cat_selected' not in context:
             context['cat_selected'] = 0
